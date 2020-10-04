15dc7482fa57a79dd7e4002cbed04b78cdbc9cac
nagaraghav <snraghav1122@gmail.com>
2020-10-03 20:39:39 -0700

    Added documentation

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Seat-Assign
+
+
+Implementation of Professor Porquet's Seat Assign Project.
+
+Python script which allows you to find students whose name or email matches a given pattern, or whose GPA is within given range.
+
+## To Run:
+python3 seats.py [command] [roster csv filename]
+
+## Implementation:
+I made the script modular and broke down the commands into 3 functions which return a 'condition' function to check if a student fits the pattern. 
+
+Once the 'condition' function has been created for the specific command, I am passing it into 'readRosterWithConditiom' function which reads the CSV file and checks if each student/row meets the condition. 
+
+I am using Python's csv library and re, regular expressions, to find pattern of email and name.
+
+## Sample Commands
+python3 seats.py "-gpa 3.2+" roster.csv
+python3 seats.py "-name cel" roster.csv
+python3 seats.py "-email \.(com|net)" roster.csv
--- seats.py
+++ seats.py
@@ -1,36 +1,45 @@
 import csv
-import sys 
+import sys
 import re
 
 
 """[summary]
+Reads the CSV file row by row, and checks if eah student meets the passed in condition or not. If it matches, prints student details to terminal.
 
+myf - condition function 
+filename - Name of CSV file of Roster
 """
+
+
 def readRosterWithCondition(myf, filename):
     with open(filename) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
 
         for i, row in enumerate(csv_reader):
-            print(i)
+
             if line_count == 0:
                 line_count += 1
             else:
+
+                # Makes sures each row has 4 columns for student details (firstname, lastname, email, gpa)
                 if(len(row) != 4):
                     print("Missing information in roster on row: ", i)
 
+                #If meets condition, print student details to terminal
                 if(myf(row)):
                     print(row)
-                
+
                 line_count += 1
 
 
 """[summary]
 Creates and returns a function to check if student's GPA is within desired range
 
-
-
+pat - pattern entered by user (gpa including + or -)
 """
+
+
 def createGpaCondition(pat):
 
     gpa = pat[:-1]
@@ -39,20 +48,25 @@ def createGpaCondition(pat):
     gpa = float(gpa)
 
     if sign == "+":
-        def gpaf(row):            
+        def gpaf(row):
             return float(row[-1]) > gpa
         return gpaf
     else:
-        def gpaf(row):            
+        def gpaf(row):
             return float(row[-1]) < gpa
         return gpaf
-    
+
+
 """[summary]
 Creates and returns a function to check if student's email matches pattern
+
+pat - regualar expression pattern entered by user 
 """
+
+
 def createEmailCondition(pat):
-    print(pat)
-    def gpaf(row): 
+
+    def gpaf(row):
         matchObj = re.search(pat, row[2].lower())
         if matchObj:
             return True
@@ -63,11 +77,16 @@ def createEmailCondition(pat):
 
 """[summary]
 Creates and returns a function to check if student's name matches pattern
+
+pat - regualar expression pattern entered by user 
+
 """
+
+
 def createNameCondition(pat):
-    
-    def gpaf(row): 
-        
+
+    def gpaf(row):
+
         matchObj_first = re.search(pat, row[0].lower())
         matchObj_last = re.search(pat, row[1].lower())
         if matchObj_first or matchObj_last:
@@ -77,8 +96,12 @@ def createNameCondition(pat):
     return gpaf
 
 
+"""[summary]
+Reads the entered arguments, creates a myCondition function and passes it on to readRosterWithCondition
 
+python3 seats.py [command] [roster csv filename]
 
+"""
 def main():
     if len(sys.argv) < 3:
         print("Too few arguments. Please pass in command and file.")
@@ -87,8 +110,7 @@ def main():
     filename = sys.argv[2]
 
     com, pattern = command.split(" ", 1)
-    
-    
+
     myCondition = None
     if(com == "-gpa"):
         myCondition = createGpaCondition(pattern)
@@ -97,14 +119,10 @@ def main():
     elif(com == "-name"):
         myCondition = createNameCondition(pattern)
     else:
-        print("No matching command.") 
+        print("No matching command.")
         return
-    
 
     readRosterWithCondition(myCondition, filename)
-
-
-
 
 
 if __name__ == "__main__":
